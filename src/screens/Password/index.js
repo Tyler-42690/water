e700869ed0dae2a1d466b4256d1eb07fe7647d68
Shark@DESKTOP-E8806R4 <sharknado8575@gmail.com>
2023-09-04 09:05:42 -0400

    Extra pass field

--- src/screens/Password/index.js
+++ src/screens/Password/index.js
@@ -1,90 +1,92 @@
-import React ,{useState}from 'react'
-import {  View, Text ,Image,StatusBar ,TouchableOpacity,StyleSheet} from 'react-native';
-import { wpxToDp,hpxToDp } from '../../utils/stylesKits';
-import { Icon,Input ,Button,} from 'react-native-elements'
+import React, { useState } from 'react';
+import { View, Text, Image, StatusBar, TouchableOpacity, StyleSheet } from 'react-native';
+import { wpxToDp, hpxToDp } from '../../utils/stylesKits';
+import { Icon, Input, Button } from 'react-native-elements';
 import axios from 'axios';
-//import Toast from 'teaset/components/Toast/Toast';
+//import { useNavigation } from '@react-navigation/native';
 
-const Password = () =>
-{
+const Password = ({route,navigation}) => {
+  const [useremail, setUseremail] = useState('');
+  const [password, setPassword] = useState('');
+  const [newpassword, setNewPassword] = useState('');
+  const [newpassword2, setNewPassword2] = useState('');
+  const [passwordsMatch, setPasswordsMatch] = useState(true); // Track if passwords match
+  //const navigation = useNavigation();
+  const resetPassword = () => {
+    if (newpassword !== newpassword2) {
+      setPasswordsMatch(false);
+      return; // Don't proceed if passwords don't match
+    }
 
-    const [useremail,setUseremail] = useState('')
-    const [password, setPassword] = useState('')
-    const [newpassword,setNewPassword] = useState('')
-  
-    return (
-      <View style={{
-        alignItems: 'center',
-        }}>
-  
-      <Input
-        placeholder='Input Email'
-        inputStyle={{color:"#333"}}
-        onChangeText={text => setUseremail(text)}
-        leftIcon={
-          <Icon
-            name='email'
-            size={24}
-          />
+    // Reset the error state if passwords match
+    setPasswordsMatch(true);
+
+    console.log(useremail);
+    console.log(password);
+    axios
+      .get("http://47.89.252.2:5000/resetpassword.php?!" + useremail + "|" + password + "|" + newpassword)
+      .then(
+        (response) => {
+          if (response.data['success'] === 1) {
+            console.log('Success');
+            navigation.navigate('Login');
+          } else {
+            console.log(response.data['error']);
+          }
+        },
+        (error) => {
+          console.log('Error');
         }
-       />
-  
+      );
+  };
+
+  return (
+    <View style={{ alignItems: 'center' }}>
       <Input
-        placeholder='Input Password'
-        secureTextEntry={true}
-        onChangeText={text => setPassword(text)}
-        leftIcon={
-          <Icon
-            name='lock'
-            size={24}    
-        />
-        }
+        placeholder="Input Email"
+        inputStyle={{ color: "#333" }}
+        onChangeText={(text) => setUseremail(text)}
+        leftIcon={<Icon name="email" size={24} />}
       />
 
       <Input
-        placeholder='Input New Password'
+        placeholder="Input Password"
         secureTextEntry={true}
-        onChangeText={text => setNewPassword(text)}
-        leftIcon={
-          <Icon
-            name='lock'
-            size={24}    
-        />
-        }
+        onChangeText={(text) => setPassword(text)}
+        leftIcon={<Icon name="lock" size={24} />}
       />
-  
-  
+
+      <Input
+        placeholder="Input New Password"
+        secureTextEntry={true}
+        onChangeText={(text) => setNewPassword(text)}
+        leftIcon={<Icon name="lock" size={24} />}
+      />
+      <Input
+        placeholder="Input New Password Again"
+        secureTextEntry={true}
+        onChangeText={(text) => setNewPassword2(text)}
+        leftIcon={<Icon name="lock" size={24} />}
+        inputStyle={passwordsMatch ? {} : { borderColor: 'red' }} // Set border color if passwords don't match
+      />
+
+      {!passwordsMatch && (
+        <Text style={{ color: 'red', marginTop: 10 }}>Passwords must match.</Text>
+      )}
+
       <View>
-        <Button buttonStyle={{borderRadius: 20,marginTop:10}}
-        title="Reset Password"
-        onPress={() => {
-          console.log(useremail)
-          console.log(password)
-          //navigation.navigate('Home')
-          axios.get("http://47.89.252.2:5000/resetpassword.php?!="+useremail+"|"+password+"|"+newpassword).then(
-            response => {
-                if(response.data['success']===1){
-                  console.log('success')
-                }else {
-                  console.log(response.data['error'])
-              }
-            },
-            error => {
-              console.log('error');
-            }
-          )
-         
-      }}
+        <Button
+          buttonStyle={{ borderRadius: 20, marginTop: 10 }}
+          title="Reset Password"
+          onPress={resetPassword}
         />
- 
       </View>
-      
-      </View>
-    );
-  
+    </View>
+  );
 };
 
 export default Password;
+
 
 const styles = StyleSheet.create({
   header: {
